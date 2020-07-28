ba4182483bf79899f40b8b9523db7b8731c77860
dlgchg <curmido@gmail.com>
2020-07-28 11:41:58 +0800

    新建项目, banner组件

---
+++ components/banner/index.js
@@ -0,0 +1,45 @@
+// components/banner/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    bannerHeight: {
+      type: Number,
+      value: 220
+    },
+    home: {
+      type: Boolean,
+      value: true
+    },
+    bannerData: {
+      type: Array,
+      value: [
+        {
+          image: "https://s2.luckincoffeecdn.com/luckywebrm/images/index/cover/bg-2.jpg"
+        },
+        {
+          image: "https://s2.luckincoffeecdn.com/luckywebrm/images/index/product/bg.jpg"
+        }
+      ]
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    currentIndex: 0
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    swiperChange: function(event) {
+      this.setData({
+        currentIndex: event.detail.current
+      })
+    }
+  }
+})
---
+++ components/skeleton/skeleton_banner/index.js
@@ -0,0 +1,26 @@
+// components/skeleton/skeleton_banner/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    radius: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
