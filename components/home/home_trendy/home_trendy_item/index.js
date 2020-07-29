ace4f084f947833e9edeb0c66d38316224e828a2
dlgchg <curmido@gmail.com>
2020-07-29 16:20:46 +0800

    首页: 潮品组件,购物车组件,署名组件

---
+++ components/home/home_trendy/home_trendy_item/index.js
@@ -0,0 +1,31 @@
+// components/home/home_trendy/home_trendy_item/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    itemData: {
+      type: Object,
+      value: {
+        image: "/image/test/trendy_i1.png",
+        name: "英伦温暖马克杯·蓝",
+        price: 88,
+        num: 701
+      }
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ components/home/home_trendy/index.js
@@ -0,0 +1,60 @@
+// components/home/home_trendy/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+   
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    trendyData: [
+      {
+        image: "/image/test/trendy_i1.png",
+        name: "英伦温暖马克杯·蓝",
+        price: 88,
+        num: 701
+      },
+      {
+        image: "/image/test/trendy_i2.png",
+        name: "英伦温暖马克杯·红",
+        price: 88,
+        num: 701
+      },
+      {
+        image: "/image/test/trendy_i.png",
+        name: "关名陶瓷内壁咖啡杯·白",
+        price: 258,
+        num: 701
+      },
+      {
+        image: "/image/test/trendy_i3.png",
+        name: "关名陶瓷内壁咖啡杯·蓝",
+        price: 258,
+        num: 701
+      },
+      {
+        image: "/image/test/trendy_i4.png",
+        name: "肖战定制款「小赞杯」",
+        price: 88,
+        num: 701
+      },
+      {
+        image: "/image/test/trendy_i5.png",
+        name: "雪人双层陶瓷杯",
+        price: 88,
+        num: 701
+      }
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
